0d0c8960ca0a6587a6690f684be55f53c32f56b5
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-09-27 19:10:06 +0600

    done the sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleFileInput = (ev: any) => {
+        console.log(ev.target.files[0])
+    }
+    return (
+        <div className="App">
+            <h3>Upload a file here</h3>
+            {/*<div className="InputContainer">*/}
+
+            {/*    <input onInput={handleFileInput} type={'file'}/>*/}
+            {/*</div>*/}
+            <div className="Viewer">
+                <div className="Left">
+                    <h4>Left Preview</h4>
+                    <div className="InputContainer">
+                        <input id={'Left-Upload'} onInput={handleFileInput} type={'file'}/>
+                    </div>
+                    <textarea className={'TextArea'}>XML Preview</textarea>
+                </div>
+                <div className="Right">
+                    <h4>Left Right</h4>
+                    <div className="InputContainer">
+                        <input id={'Right-Upload'} onInput={handleFileInput} type={'file'}/>
+                    </div>
+                    <textarea className={'TextArea'}>XML Preview</textarea>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
